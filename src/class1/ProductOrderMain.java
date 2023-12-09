b9fcef47b55eee7b69cc614de3b50691878cf78a
196code-gray <doco827@gmail.com>
2023-12-09 11:59:02 +0900

    [문제] 상품 주문 정보 출력하기

---
+++ src/class1/ProductOrderMain.java
@@ -0,0 +1,27 @@
+package class1;
+
+public class ProductOrderMain {
+    public static void main(String[] args) {
+        ProductOrder po1 = new ProductOrder();
+        ProductOrder po2 = new ProductOrder();
+        ProductOrder po3 = new ProductOrder();
+
+        po1.productName = "두부";
+        po2.productName = "김치";
+        po3.productName = "콜라";
+        po1.price = 2000;
+        po2.price = 5000;
+        po3.price = 1500;
+        po1.quantity = 2;
+        po2.quantity = 1;
+        po3.quantity = 2;
+
+        ProductOrder[] productOrders = new ProductOrder[] {po1, po2, po3};
+        int totle = 0;
+        for (ProductOrder p : productOrders) {
+            System.out.println("상품명: " + p.productName + " 가격: " + p.price + " 수량: " + p.quantity);
+            totle += p.price * p.quantity;
+        }
+        System.out.println("총 결제 금액: " + totle);
+    }
+}
