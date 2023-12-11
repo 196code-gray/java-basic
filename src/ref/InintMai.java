133c60e99b3b73d43658bd7525897f07fcb10c60
196code-gray <doco827@gmail.com>
2023-12-11 12:01:09 +0900

    null을 확인하기 위한 클래스

---
+++ src/ref/InintMai.java
@@ -0,0 +1,9 @@
+package ref;
+
+public class InintMai {
+    public static void main(String[] args) {
+        IntiData data = new IntiData();
+        System.out.println(data.value1);
+        System.out.println(data.value2);
+    }
+}
---
+++ src/ref/IntiData.java
@@ -0,0 +1,6 @@
+package ref;
+
+public class IntiData {
+    int value1;
+    int value2 = 10;
+}
