a87054dde2e1bb4520c2d48224ce1010c8e8d4da
196code-gray <doco827@gmail.com>
2023-12-12 11:16:22 +0900

    음악 플레이어 메서드 추출

--- src/oop1/MusicPlayerMain2.java
+++ src/oop1/MusicPlayerMain2.java
@@ -4,24 +4,43 @@ public class MusicPlayerMain2 {
     public static void main(String[] args) {
         MusicPlayerData data = new MusicPlayerData();
 
-        data.isOn = true;
-        System.out.println("음악 플레이어를 시작합니다");
+        on(data);
 
         // 볼륨 증가
-        data.volume++;
-        System.out.println("음악 플레이어 볼륨: " + data.volume);
-        data.volume++;
-        System.out.println("음악 플레이어 볼륨: " + data.volume);
+        volumeUp(data);
+        volumeUp(data);
         // 감소
-        data.volume--;
-        System.out.println("음악 플레이어 볼륨: " + data.volume);
+        volumeDown(data);
         // 플레이어 상태
+        showStatus(data);
+        // 끄기
+        off(data);
+    }
+
+    static void showStatus(MusicPlayerData data) {
         System.out.println("음악 플레이어 상태 확인");
         if (data.isOn) {
             System.out.println("음악 플레이어 ON, 볼륨:" + data.volume);
         } else System.out.println("음악 플레이어 OFF");
-        // 끄기
+    }
+
+    static void volumeDown(MusicPlayerData data) {
+        data.volume--;
+        System.out.println("음악 플레이어 볼륨: " + data.volume);
+    }
+
+    static void volumeUp(MusicPlayerData data) {
+        data.volume++;
+        System.out.println("음악 플레이어 볼륨: " + data.volume);
+    }
+
+    static void off(MusicPlayerData data) {
         data.isOn = false;
         System.out.println("음악 플레이어를 종료합니다.");
+    }
+
+    static void on(MusicPlayerData data) {
+        data.isOn = true;
+        System.out.println("음악 플레이어를 시작합니다");
     }
 }
