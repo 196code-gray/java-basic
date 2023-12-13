e5b7929cb574a85838368783a1cf2a3918dfbabd
196code-gray <doco827@gmail.com>
2023-12-13 16:12:21 +0900

    생성자 사용해보기

---
+++ src/construct/ConstructMain.java
@@ -0,0 +1,16 @@
+package construct;
+
+public class ConstructMain {
+    public static void main(String[] args) {
+        MemberConstruct memberConstruct = new MemberConstruct("user1", 11,10);
+        MemberConstruct memberConstruct1 = new MemberConstruct("user2", 15);
+
+
+        }
+    }
+    /*
+    생성자 사용 이유
+    객체 생성 후 메서드를 직접 불러줘야 되는 불편함이 사라짐
+    개발자가 값을 안넣어줄 가능성이 있음
+     */
+
