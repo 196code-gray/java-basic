13523f40e991071940addf36b4c8b88592a4081b
196code-gray <doco827@gmail.com>
2023-12-13 16:11:41 +0900

    클래스 생성자 적용 전 & 적용 후

---
+++ src/construct/MethodInitMain2.java
@@ -0,0 +1,22 @@
+package construct;
+
+public class MethodInitMain2 {
+    public static void main(String[] args) {
+        MemberInit member1 = new MemberInit();
+        initMember(member1, "user1", 15, 90);
+
+        MemberInit member2 = new MemberInit();
+        initMember(member2, "user2", 16, 80);
+
+        MemberInit[] members = {member1, member2};
+
+        for (MemberInit m : members) {
+            System.out.println("이름: " + m.name + " 나이 : " + m.age + " 성적: " + m.grade);
+        }
+    }
+    static void initMember(MemberInit memberInit, String name, int age, int grade) {
+        memberInit.name = name;
+        memberInit.age = age;
+        memberInit.grade = grade;
+    }
+}
