3afe9603779fbb0b83a12b1529445d7114e91e69
196code-gray <doco827@gmail.com>
2024-03-15 09:02:28 +0900

    static 변수 인스턴스로 접근

--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -10,5 +10,18 @@ public class DataCountMain3 {
 
         Data3 data3 = new Data3("C");
         System.out.println("C count=" + Data3.count);
+
+        Data3 data4 = new Data3("D");
+        System.out.println("D count=" + data4.count);
+
+        /*
+        인스턴스를 통한 접근은 권장 x
+        why?
+          인스턴스 변수인지 헷갈릴 가능성 o
+
+          개발할 때 만드는 입장 / 가져다 쓰는 입장 2가지 생각
+          가져다 쓰는 입장을 생각하며 코드 작성해야 함.
+         */
+
     }
 }
