63d91ea44697761f52a790bb5b639964e4cabe88
196code-gray <doco827@gmail.com>
2024-03-16 13:47:49 +0900

    static 수정

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,9 +1,14 @@
 package static2;
 
+/*
+import static 메서드를 적으면 클래스명 생략하고 사용 가능
+ */
+import static static2.DecoData.staticCall;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -13,6 +18,13 @@ public class DecoDataMain {
         DecoData data2 = new DecoData();
         data2.instanceCall();
 
-        DecoData.staticCall(data1);
+        staticCall(data1);
+
+        DecoData data3 = new DecoData();
+        // 인스턴스를 통해서 접근 가능하지만 권장 x
+        // 다른 개발자가 봤을 때 인스턴스 메서드라고 생각할 수 있음.
+        staticCall();
+
+        staticCall();
     }
 }
