8375af59c690127f6fdf3897eff60e2aa55f1882
196code-gray <doco827@gmail.com>
2024-03-21 08:30:59 +0900

    접근제어자에 따른 상속 관계 메서드 이용

---
+++ src/extends1/access/child/Child.java
@@ -0,0 +1,18 @@
+package extends1.access.child;
+
+import extends1.access.parent.Parent;
+
+public class Child extends Parent {
+    public void call(){
+        publicValue = 1;
+        protectedValue = 1;
+//        defaultValue
+//        privateValue
+
+        publicMethod();
+        protectedMethod();
+
+        printParent();
+    }
+
+}
