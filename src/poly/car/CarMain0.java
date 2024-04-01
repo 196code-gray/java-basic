40037e86778e82fe0e6e56b64f0c50b240edb43d
196code-gray <doco827@gmail.com>
2024-04-01 09:21:59 +0900

    예제 -  새로운 자동차를 생성할 때마다 많은 로직 수정 발생

--- src/poly/car/CarMain0.java
+++ src/poly/car/CarMain0.java
@@ -7,5 +7,10 @@ public class CarMain0 {
 
         driver.setK3Car(k3Car);
         driver.drive();
+        // 자동차가 하나 추가 될 때마다 로직 변경이 너무 많이 일어남.
+        Model3Car model3Car  = new Model3Car();
+        driver.setK3Car(null);
+        driver.setModel3Car(model3Car);
+        driver.drive();
     }
 }
